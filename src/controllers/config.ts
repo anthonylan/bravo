0aa2438205e965de88f2a7fb175531fe74d82b98
Anthony Charles <anthonylannn@gmail.com>
2024-11-25 15:08:25 +0100

    new custom domain

--- src/controllers/config.ts
+++ src/controllers/config.ts
@@ -1,6 +1,6 @@
 const baseUrl = window.location.origin.includes('localhost')
 ? 'http://localhost:8888/api'
-: 'https://bravo.anthonycharles.work/api'; //change this to your server URL
+: 'https://bravo.indiebold.com/api'; //change this to your server URL
 
 
 
